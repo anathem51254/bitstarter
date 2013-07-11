ef5d736960490916c8568237b5cc1d5ea3565828
Anathem <ravel.ford@hotmail.com>
2013-07-11 20:42:52 +1000

    Updated index.html and web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var index = fs.readFileSync('index.html');
+
+var indexString = index.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexString);
 });
 
 var port = process.env.PORT || 5000;
